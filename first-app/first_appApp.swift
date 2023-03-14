c04ca9d45cea2285b8dfd275d76d4f8f0037cb4a
Matias <mpintov42@gmail.com>
2023-03-14 19:29:23 -0300

    Initial Commit

---
+++ first-app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first-app
+//
+//  Created by Matías Pinto on 14-03-23.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
